c2db0ba8d5d0c132ed568ac221882cfb8cecc504
ppomarswfs <pere.pomar@smallworldfs.com>
2022-11-22 13:49:58 +0100

    Remove RESULTS ARRAY

--- src/main/java/com/smallworldfs/tennis/TennisGame.java
+++ src/main/java/com/smallworldfs/tennis/TennisGame.java
@@ -1,54 +1,65 @@
 package com.smallworldfs.tennis;
 
+import java.text.MessageFormat;
+
 public class TennisGame
 
 {
 
     private static final String[] POINTS = {"Love", "Fifteen", "Thirty", "Forty"};
-    private static final String[] RESULTS = {"Deuce", "Advantage ", "Win for "};
     private static final String PLAYER_ONE = "player1";
     private static final String ALL = "-All";
+    private static final String DEUCE = "Deuce";
+    private static final String ADVANTAGE = "Advantage ";
+    private static final String WIN = "Win for ";
+
     private int playerOne = 0;
     private int playerTwo = 0;
-
 
     public TennisGame(String player1Name, String player2Name) {}
 
     public String getScore() {
-        if (isWinnerOrDeuce()) {
-            return getMessageFromResults();
+        if (isWinnerOrDeuceOrAdvance()) {
+            return getMessageWithoutPoints();
         }
         return getMessageFromPoints();
     }
-    private boolean isWinnerOrDeuce() {
+
+    private boolean isWinnerOrDeuceOrAdvance() {
         return playerOne > 3 || playerTwo > 3 || (playerOne == 3 && playerTwo == 3);
     }
 
-    private String getMessageFromResults() {
-        int difference = playerOne - playerTwo;
-        return RESULTS[Math.min(2, Math.abs(difference))] + getWinningPlayer(difference);
-    }
-    private String getWinningPlayer(int difference) {
+    private String getMessageWithoutPoints() {
         if (isTie()) {
-            return "";
+            return DEUCE;
+        } else if (isAdvance()) {
+            return ADVANTAGE + getWinningPlayer();
         }
-        return "player" + (isWinningPlayerOne(difference) ? 1 : 2);
+        return WIN + getWinningPlayer();
+    }
+
+    private String getWinningPlayer() {
+        return "player" + (isWinningPlayerOne() ? 1 : 2);
+    }
+
+    private boolean isAdvance() {
+        return Math.abs(playerOne - playerTwo) == 1;
     }
 
     private String getMessageFromPoints() {
         if (isTie()) {
             return POINTS[playerOne] + ALL;
         }
-        return POINTS[playerOne] + "-" + POINTS[playerTwo];
+        return String.join("-", POINTS[playerOne], POINTS[playerTwo]);
     }
 
     private boolean isTie() {
         return playerOne == playerTwo;
     }
-    private boolean isWinningPlayerOne(int difference) {
-        return difference > 0;
-    }
 
+    private boolean isWinningPlayerOne() {
+        return playerOne > playerTwo;
+    }
 
     public void wonPoint(String player) {
         if (PLAYER_ONE.equals(player)) {
